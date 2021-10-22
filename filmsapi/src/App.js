629a5a2bb0ee38e79f72e96fadc9bd20219d560f
dalshenekuda <51684531+dalshenekuda@users.noreply.github.com>
2021-10-22 05:42:23 +0300

    adaptability + code cleaning

--- filmsapi/src/App.js
+++ filmsapi/src/App.js
@@ -1,5 +1,4 @@
 import './style.scss';
-// import {getFilms} from './functions/requests'
 import React, {useEffect, useState} from 'react'
 import Axios from "axios";
 import {Film} from "./components/Film";
@@ -8,25 +7,43 @@ import {Details} from "./components/Details";
 function App() {
 
     const [films, setFilms] = useState([])
-    const [currentFilm, setCurrentFilm] = useState(0)
+    const [currentFilm, setCurrentFilm] = useState('')
+    const [favoritesList, setFavoritesList] = useState(JSON.parse(localStorage.getItem('favoritesList')) ?
+        JSON.parse(localStorage.getItem('favoritesList')) : [])
+    const [alreadyExist, setAlreadyExist] = useState(false)
 
-    // const [forwardList, setForwardList] = useState(JSON.parse(localStorage.getItem('forwardList'))?
-    //     JSON.parse(localStorage.getItem('forwardList')):[])
+    const addToFavorites = (newFilm) => {
+        let contains = false
+        if (favoritesList.length > 0) {
+            for (let i = 0; i < favoritesList.length; i++) {
+                if (favoritesList[i].title === newFilm.title) {
+                    contains = true
+                    setAlreadyExist(true)
+                    break;
+                }
+            }
+        }
 
-    // const addToForwards = (newEl) => {
-    //     setForwardList([...forwardList,newEl])
-    //     // console.log(forwardList)
-    //     localStorage.setItem('forwardList', JSON.stringify(forwardList));
-    // }
+        if (!contains) {
+            const prev = favoritesList
+            prev.push(newFilm)
+            setFavoritesList([...prev])
+            localStorage.setItem('favoritesList', JSON.stringify(favoritesList))
+        }
+    }
+
     const openDetails = (newCurFilm) => {
         setCurrentFilm(newCurFilm)
     }
+
+    useEffect(() => {
+        setAlreadyExist(false)
+    }, [currentFilm])
 
     useEffect(async () => {
         try {
             const {data} = await Axios.get("https://swapi.dev/api/films")
             setFilms(data.results)
-            console.log('few')
         } catch (e) {
             console.log(e)
         }
@@ -38,29 +55,30 @@ function App() {
             <h1>The Star Wars API</h1>
 
             <div className="content">
-                <div className="content__list">
-                    {/* <a className="content__list__head-font">Titles</a> */}
-
-                    <div className="content__list__films">
-                        {films ?
-                            (films.map((film) => (
-                                    <Film key={film.title} openDetails={openDetails} film={film}/>
-                                )
-                            )) :
-                            (<div></div>)}
+                <div className="content__box">
+                    <div className="width40">
+                        <a className="content__box__head-font">Titles</a>
+                        <div className="content__box__list">
+                            {films && films.map((film) =>
+                                <Film key={film.title} openDetails={openDetails} film={film}/>)}
+                        </div>
                     </div>
 
-                    <div className="content__list__details">
-                        {currentFilm ?
-                            (<Details film={currentFilm}/>)
-                            :
-                            (<div></div>)}
-
+                    <div className="width60">
+                        <a className="content__box__head-font">Details</a>
+                        <div className="content__box__list-details">
+                            {currentFilm &&
+                            <Details film={currentFilm} alreadyExist={alreadyExist} addToFavorites={addToFavorites}/>}
+                        </div>
                     </div>
-
                 </div>
-                <div className="content__details">
 
+                <div className="favorites-box">
+                    <a className="content__box__head-font">Favorites list</a>
+                    <div className="content__box__list-details red">
+                        {favoritesList && favoritesList.map((film) =>
+                            (<Film openDetails={openDetails} film={film}/>))}
+                    </div>
                 </div>
             </div>
         </div>
--- filmsapi/src/components/Details.js
+++ filmsapi/src/components/Details.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
-export const Details =(props)=>{
-    return(
+export const Details = (props) => {
+    return (
         <div className="details">
             <a className="details__font bold">{props.film.title}</a>
             <a className="details__font">{props.film.opening_crawl}</a>
             <p className="details__font"><span className="bold">Director: </span> {props.film.director}</p>
             <p className="details__font"><span className="bold">Producer: </span> {props.film.producer}</p>
             <p className="details__font"><span className="bold">Release date: </span> {props.film.release_date}</p>
+
+            <div className="details__add-elements">
+                <button onClick={() => props.addToFavorites(props.film)}>Add to favorites</button>
+                {props.alreadyExist && <div><a className="details__add-elements__text">Already in favorites</a></div>}
+            </div>
         </div>
-    )  
+    )
 }
